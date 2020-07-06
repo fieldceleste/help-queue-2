79bb3312aef4ae43376027ad1e72e7d5811d3f2f
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-07-06 16:23:38 -0700

    pass all three  test for index-reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+//main hub for all reducers
+
+import formVisibleReducer from './form-visible-reducer';
+import ticketListReducer from './ticket-list-reducer';
+import { combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterTicketList: ticketListReducer
+});
+
+export default rootReducer;
