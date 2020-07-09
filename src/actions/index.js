72c5c2a0b3bcadf02f4c0258f5ca23de8acac855
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-07-08 20:52:11 -0700

    pass third and fourth test for index.test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,27 @@
+import * as c from "./../actions/ActionTypes";
+
 export const deleteTicket = id => ({
-  type: 'DELETE_TICKET',
+  type: "DELETE_TICKET",
   id
 });
 export const toggleForm = () => ({
-  type: 'TOGGLE_FORM'
+  type: "TOGGLE_FORM"
 });
 export const addTicket = (ticket) => {
-  const { names, location, issue, id } = ticket;
+  const { names, location, issue, id,formattedWaitTime, timeOpen } = ticket;
   return {
-    type: 'ADD_TICKET',
+    type: c.ADD_TICKET,
     names: names,
     location: location,
     issue: issue,
-    id: id
-  }
-}
+    id: id,
+    formattedWaitTime,
+    timeOpen: timeOpen
+  };
+};
+
+export const updateTime = (id, formattedWaitTime) => ({
+  type: c.UPDATE_TIME,
+  id: id,
+  formattedWaitTime: formattedWaitTime,
+});
